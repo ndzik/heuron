c06e5e94a6266e25eb259add171d9b22511e99ec
Norbert Dzikowski <norbert@perun.network>
2023-05-30 15:11:00 +0200

    Heuron/V1/Batched: Define sane default exports for library interface

--- src/Heuron/V1/Batched.hs
+++ src/Heuron/V1/Batched.hs
@@ -1,9 +1,29 @@
 module Heuron.V1.Batched
-  ( module Heuron.V1.Batched.Network,
+  ( ActivationFunction,
+
+    -- * Layer & Lenses
+    Layer (..),
+    weights,
+    bias,
+    activation,
+
+    -- * Network
+    Network (..),
+    (=|),
+
+    -- * Network primitives
+    reverseNetwork,
     module Heuron.V1.Batched.Input,
+
+    -- * Forward propagation
+    Forward (..),
+
+    -- * Loss primitives
+    categoricalCrossEntropy,
   )
 where
 
+import Heuron.V1.Batched.Forward
 import Heuron.V1.Batched.Input
+import Heuron.V1.Batched.Loss
 import Heuron.V1.Batched.Network
-
