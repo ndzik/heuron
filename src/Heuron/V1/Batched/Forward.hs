c9c2296a5d400ae8cd13be67834cea1107dd230d
Norbert Dzikowski <norbert@perun.network>
2023-06-19 11:32:54 +0200

    Heuron/V1/Batched: Let forward pass generate an updated network

--- src/Heuron/V1/Batched/Forward.hs
+++ src/Heuron/V1/Batched/Forward.hs
@@ -4,14 +4,17 @@ module Heuron.V1.Batched.Forward where
 
 import Control.Lens
 import GHC.TypeLits
+import Heuron.V1.Batched.Activation
 import Heuron.V1.Batched.Input
+import Heuron.V1.Batched.Layer
 import Heuron.V1.Batched.Network
 import Linear.Matrix
 
 -- | Forward is the typelevel interpreter for our constructed network. It will
--- generate the correct amounts of forward calls for each layer.
+-- generate the correct amounts of forward calls for each layer and return the
+-- updated network together with the final output.
 class Forward as where
-  forward :: as -> InputOf as -> FinalOutputOf as
+  forward :: as -> InputOf as -> (as, FinalOutputOf as)
 
 -- | Recursion stop for Forward. Networks are at least two layers deep, input
 -- and output layer.
@@ -22,11 +25,16 @@ instance
     KnownNat j,
     KnownNat k,
     KnownNat b,
-    Compatible (Layer i n) '[Layer j k]
+    ActivationFunction af,
+    ActivationFunction af',
+    Compatible (Layer b i n af op) '[Layer b j k af' op']
   ) =>
-  Forward (Network b '[Layer i n, Layer j k])
+  Forward (Network b '[Layer b i n af op, Layer b j k af' op'])
   where
-  forward (pl :>: ll :>: NetworkEnd) i = forwardInput ll . forwardInput pl $ i
+  forward (pl :>: ll :>: NetworkEnd) i =
+    let resPl = forwardInput pl i
+        resLl = forwardInput ll resPl
+     in (pl {_input = i} :>: ll {_input = resPl} :>: NetworkEnd, resLl)
 
 -- | Recursion step for Forward. This will construct a chain of forward calls
 -- that pass the output of the previous layer to the next layer.
@@ -35,22 +43,32 @@ instance
     KnownNat i,
     KnownNat n,
     KnownNat b,
-    Compatible (Layer i n) as,
-    InputOf (Network b as) ~ Input b n Double,
-    FinalOutputOf (Network b as) ~ FinalOutputOf (Network b (Layer i n : as))
+    KnownNat b',
+    b ~ b',
+    ActivationFunction af,
+    Compatible (Layer b' i n af op) as,
+    InputOf (Network b as) ~ Input b' n Double,
+    FinalOutputOf (Network b as) ~ FinalOutputOf (Network b (Layer b' i n af op : as))
   ) =>
-  Forward (Network b (Layer i n : as))
+  Forward (Network b (Layer b' i n af op : as))
   where
-  forward (l :>: ls) = forward ls . forwardInput l
+  forward (l :>: ls) i =
+    let resL = forwardInput l i
+        (ls', res) = forward ls resL
+     in (l {_input = i} :>: ls', res)
 
-forwardInput :: (KnownNat m, KnownNat n, KnownNat i) => Layer i n -> Input m i Double -> Input m n Double
+forwardInput ::
+  (KnownNat n, KnownNat i, KnownNat b, ActivationFunction af) =>
+  Layer b i n af op ->
+  Input b i Double ->
+  Input b n Double
 forwardInput s inputs =
   -- This does for every neuron i (row) in the layer:
   -- > Σ(w_ij * x_j) + b_i
   -- where `i` is the neuron index and `j` is the input index.
   let transposedNeurons = s ^. weights . to transpose
       weightedInputs = (inputs !*! transposedNeurons) <&> (+ (s ^. bias))
-      activationFunction = s ^. activation
+      af = s ^. activationFunction . to activation
    in -- Apply activation and return result for this layer for each observation
       -- in input set.
-      (activationFunction <$>) <$> weightedInputs
+      (af <$>) <$> weightedInputs
