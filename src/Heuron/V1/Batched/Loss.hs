73cf8b226208538b904a1c51de4dd56233e7c770
Norbert Dzikowski <norbert@perun.network>
2023-07-10 19:27:52 +0200

    Heuron/V1/Batched/Loss: Add accurator capability to LossComparator

--- src/Heuron/V1/Batched/Loss.hs
+++ src/Heuron/V1/Batched/Loss.hs
@@ -1,5 +1,7 @@
 module Heuron.V1.Batched.Loss where
 
+import Data.Data (Proxy (..))
+import Data.Vector (foldl', maxIndex)
 import GHC.TypeLits
 import Heuron.Functions (mergeEntriesWith)
 import Heuron.V1.Batched.Activation (Differentiable (..))
@@ -15,10 +17,27 @@ class (Differentiable f) => LossComparator f where
   -- index.
   losser :: (KnownNat b, KnownNat n) => f -> (Input b n Double -> Input b n Double -> Input b n Double)
 
+  -- | Return the accuracy function which calculates the accuracy for a given
+  -- set of predictions compared to a set of ground truths. The result is a
+  -- single value representing the accuracy of the predictions for the given
+  -- batch of samples.
+  accurator :: (KnownNat b, KnownNat n) => f -> (Input b n Double -> Input b n Double -> Double)
+
 data CategoricalCrossEntropy = CategoricalCrossEntropy
 
 instance LossComparator CategoricalCrossEntropy where
   losser CategoricalCrossEntropy = categoricalCrossEntropy
+  accurator CategoricalCrossEntropy = categoricalAccuracy
+
+-- | categoricalAccuracy calculates the accuracy for a given set of predictions
+-- compared to a set of truth values.
+categoricalAccuracy :: forall b n. (KnownNat b, KnownNat n) => Input b n Double -> Input b n Double -> Double
+categoricalAccuracy truth prediction =
+  let correctPredictions = liftI2 matchingPrediction truth prediction
+      numOfCorrectPredictions = foldr (\p acc -> if p then acc + 1 else acc) 0 correctPredictions
+   in numOfCorrectPredictions / fromIntegral (natVal (Proxy @b))
+  where
+    matchingPrediction t p = (maxIndex . toVector $ t) == (maxIndex . toVector $ p)
 
 -- | categoricalCrossEntropy (CCE) calculates the loss for a given set of
 -- predictions compared to a set of truth values.
