959c5d3c12e964f8f7f6e027b839b4d427511ad7
Norbert Dzikowski <lambdax.one@icloud.com>
2024-12-30 19:59:54 +0100

    app: fix monomer version and GHC version + base library

--- app/Main.hs
+++ app/Main.hs
@@ -8,5 +8,5 @@ module Main where
 
 import V2Example
 
-main :: forall pixelCount batchSize numOfImages hiddenNeuronCount. (hiddenNeuronCount ~ 16, pixelCount ~ 784, batchSize ~ 100, numOfImages ~ 57321) => IO ()
+main :: forall pixelCount batchSize numOfImages hiddenNeuronCount. (hiddenNeuronCount ~ 16, pixelCount ~ 784, batchSize ~ 100, numOfImages ~ 60000) => IO ()
 main = executeV2Network @pixelCount @batchSize @numOfImages @hiddenNeuronCount
